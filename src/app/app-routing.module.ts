df41c318e5fb47c17e19d3dc0eae274ee8fbd437
Matthew Pekarek <matthewp131@gmail.com>
2018-05-27 21:26:07 -0500

    Added a navbar with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'clippy',
-    loadChildren: './agent-maker/agent-maker.module#AgentMakerModule'
+    loadChildren: './agent-maker/agent-maker.module#AgentMakerModule',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: ''
   }
 ];
 
